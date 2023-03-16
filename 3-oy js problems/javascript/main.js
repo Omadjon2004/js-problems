0bfaaed1e78129f54ebf4db654fc64b4d8b37286
Omadjon2004 <122809872+Omadjon2004@users.noreply.github.com>
2023-03-16 20:00:48 +0500

    Add files via upload

---
+++ 3-oy js problems/javascript/main.js
@@ -0,0 +1,129 @@
+             //FIRST TASK  
+
+
+//   const firstItem=prompt('Biron qiymat kiriting')
+  
+//   console.log(firstItem);
+//   const secondItem=prompt('Biron qiymat kiriting')
+//   console.log(secondItem);
+//   const thirdItem=prompt(' Biron qiymat kiriting')
+//   console.log(thirdItem);
+//   const fourthItem=prompt('Biron qiymat kiriting')
+//   console.log(fourthItem);
+//   const fifthItem=prompt(' Biron qiymat kiriting')
+//    console.log(fifthItem);
+  
+//    const items=[ +firstItem,+secondItem,+thirdItem,+fourthItem,+fifthItem]
+//    console.log(items);
+// if  ( typeof firstItem === "number" && firstItem === "" && typeof secondItem === "number"  && secondItem === '' && typeof thirdItem === "number" && thirdItem === '' && typeof fourthItem === "number" && fourthItem=== '' && typeof fifthItem === "number" && fifthItem=== '') {
+//    console.log(NaN);
+// }else{
+//     let total=0
+
+//     for(num of items ){
+//      total=total + num
+//     }
+   
+
+
+//  console.log(`Barchasini yigindisi ${ total} `);
+ 
+// }
+
+
+  
+  
+  
+  
+  
+  
+                  // TASK 2
+       
+       
+//   const musicTypes=[' Jazz ',' Blues ']  
+//     console.log(musicTypes.join());
+
+
+// musicTypes.push(' Rock-n-Roll ')
+// console.log(musicTypes.join());
+
+// musicTypes[1]=' Classics '
+// console.log(musicTypes.join());
+
+// musicTypes.shift()
+// console.log(musicTypes.join());
+
+// musicTypes.unshift(' Rap ',' Reggae ')
+// console.log(musicTypes.join());
+
+
+
+
+
+
+          //TASK 3
+
+
+        
+        
+//           let salaries = {
+//             Bob : 1700,
+//             John: 6000,
+//             Evelina: 1200
+//             }
+
+//           let res = 0;
+
+//           function summ(salaries) {
+          
+//             for(let key in salaries) {
+                
+//            if(Object.hasOwnProperty.call(salaries, key)) {
+//             let num = salaries[key];
+//               res = res + num;
+// }
+              
+//           }
+    
+//              }
+             
+//              summ(salaries);     
+         
+// console.log(res)
+        
+
+
+
+
+
+                 //TASK 4
+
+// const numbers= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
+
+
+
+// let even = 0,
+//      odd = 0,
+//     result = [];
+
+// for ( let i=0 ; i < numbers.length ; i++){
+
+    
+
+//     if (numbers[i]%2 === 0) {
+
+//         even += numbers[i]
+
+//     } else{
+//       odd += numbers[i]
+//     }
+// }
+
+
+// result.push(even, odd);
+
+// console.log(result)
+
+
+
+
